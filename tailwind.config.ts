ae25fe6cb1a0ebba35bee6f964f6c2b807d67fd7
abdullatufail <mtufail.bscs23seecs@seecs.edu.pk>
2025-07-02 13:22:35 +0500

    initial

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)", "sans-serif"],
+        "perfectly-nineties": ["var(--font-perfectly-nineties)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
